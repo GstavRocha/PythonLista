7073bd309945fc0bcb37ffd33773da7aa1506bfe
Gustavo Rocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-05-20 06:02:16 -0300

    Atividade de numeros primos

--- Atividade/primos.py
+++ Atividade/primos.py
@@ -1,5 +1,10 @@
+#https://www.somatematica.com.br/fundam/primos.php#:~:text=Para%20saber%20se%20um%20n%C3%BAmero,caso%20o%20n%C3%BAmero%20%C3%A9%20primo.
 primos = int(input(" escolha um numero "))
 num = 10000
 # por decomposição
 for i in range (primos,num):
-    if primos
+    if primos % i == 0:
+        print('não é primo')
+    elif primos % 5 == 0 or primos% 5 == 5:
+        print( 'teste')
+
