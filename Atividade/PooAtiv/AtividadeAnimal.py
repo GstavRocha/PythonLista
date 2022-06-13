5e465b4df63ccccd10833ecec54d81a29118a808
Gustavo Rocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-06-13 14:26:53 -0300

    Gato

--- Atividade/PooAtiv/AtividadeAnimal.py
+++ Atividade/PooAtiv/AtividadeAnimal.py
@@ -1,9 +1,3 @@
-# Implemente as classes conforme diagrama UML (SLIDE);
-# O método esta_cansado() retorna true quando o cachorro tiver dado mais do 5 saltos.
-# O método descansar() zera os saltos do  Cachorro.
-# O método esta_na_arvore() retorna o valor do atributo naArvore
-# Os metodos latir() e miar() devem escrever na tela as mensagens relativas aos sonos.
-# O metodo construto das subclasses deve parametrizar o nome e o peso.
 
 class Animal:
     def __init__(self, peso, nome, posicao):
@@ -18,21 +12,51 @@ class Animal:
         return self._posicao
 
 class Gato(Animal):
-    def __init__(self, naArvore):
-      self.naArvore = naArvore
-      naArvore = True
+    def __init__(self, peso, nome, posicao,naArvore):
+        self._naArvore = naArvore
+        super().__init__(peso,nome,posicao)
     
-    def miar():
-        print ('Miauu....')
+    def miar(self):
+        print('Miauuu...')
+    
+    def estar_arvore(self):
+        if self._naArvore == True:
+            return 'Está na Árvore'
+        else:
+            return 'Não está na Árvore'
+    
+    def subir_arvore(self):
+        if self._naArvore == True:
+            return 'Já está na Arvore'
+        else: 
+            self._naArvore = True
+            return "Subiu na arvore"
+    def descer_arvore(self):
+        if self.estar_arvore == True:
+            self.estar_arvore == False
+            return "Desceu da Arvore"
+        else:
+            return "Ele já está embaixo"
 
-tobinho = Animal(12,'Tobinho',2);
-tobinho.moverse()
-tobinho.moverse()
-tobinho.moverse()
-tobinho.moverse()
-tobinho.moverse()
-tobinho.moverse()
-print(tobinho.local())
+class Cachorro(Animal):
+    def __init__(self,peso, nome, posicao, qtd_saltos):
+        self._qtd_saltos = qtd_saltos
+        super().__init__(peso,nome, posicao)
+    
+    def latir(self):
+        print ('Au, au...')
+    def saltar(self):
+        self._qtd_saltos += 1
+    def descansar(self):
+        self._qtd_saltos = 0
 
+    def esta_cansado(self):
+        if self._qtd_saltos > 5:
+            print('Está cansado')
+            return True
+        else:
+            contarSaltos = 5 - self._qtd_saltos
+            print( f'Ele ainda tem {contarSaltos} de saltos')
 
-//
+chanim = Gato(10,"Chanim",2,False)
+lucky = Cachorro(25,'Lucky',2,2)
