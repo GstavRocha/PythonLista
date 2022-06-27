d6780d062cab810f725c4bbedeb389fad9899dea
GstavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-06-26 21:45:04 -0300

    Listas

---
+++ Atividade/PooAtiv/singleList.py
@@ -0,0 +1,14 @@
+# Atividade de Listas
+# lista = [1,2,3,4,5,6,7]
+# print(lista[::-1])
+# print(lista[1::-1])
+# print(lista[:-3:-1])
+
+# for i in range(len(lista)):
+#     print(lista[:-7], end=',')
+
+nota1  = [1,2,3,4,5,6,7,8,9]
+print(nota1)
+nota2 = nota1 
+nota1[0]= 18 # muda tanto para nota1 quanto para nota2.
+print(nota2)
